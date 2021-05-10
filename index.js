f3b4fbcceabbf7a824501f6d07189ed92dd76d97
Jopce <jokubass.petraitis@gmail.com>
2021-05-10 20:54:15 +0300

    Rough floor changing, still in progress

---
+++ index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // make buttons call function that changes the visible floor
+    document
+        .querySelector("#floor1button")
+        .addEventListener("click", () => change_floor("floor1"));
+
+    document
+        .querySelector("#floor2button")
+        .addEventListener("click", () => change_floor("floor2"));
+
+    document
+        .querySelector("#floor3button")
+        .addEventListener("click", () => change_floor("floor3"));
+
+    // by default load 2nd floor
+    change_floor("floor2");
+});
+
+// change floor, input is floor div id
+function change_floor(floor) {
+    console.log("yay");
+    document.querySelector("#floor1").style.display = "none";
+    document.querySelector("#floor2").style.display = "none";
+    document.querySelector("#floor3").style.display = "none";
+
+    document.querySelector(`#${floor}`).style.display = "block";
+}
