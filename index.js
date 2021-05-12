e8dd6820b25feb1d67c0e401610f6b1150c8fbcd
MatasSioma <matassemenas@gmail.com>
2021-05-12 16:21:30 +0300

    Added color, good lokin svg, window pop-up WIP

--- index.js
+++ index.js
@@ -14,6 +14,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // by default load 2nd floor
     change_floor("floor2");
+
+
+    //change color and window pop up
+    document.querySelectorAll(".st0").forEach(function(room) {
+        //room.addEventListener("click", () => info(this.dataset.name)) neveikia
+        room.onclick = function(event) {
+            const fillcolor = "rgb(64, 43, 122)"; //vvdgpurple
+            //clear all color
+            document.querySelectorAll(`polygon[style="fill: ${fillcolor};"], path[style="fill: ${fillcolor};"]`).forEach(function(element) {
+                element.removeAttribute("style");
+            });
+
+            const data = event.target.dataset;
+            //create a window on the left with collected data from the dataset
+            info(data.name);
+            event.target.style.fill = fillcolor;
+        }
+    })
+
 });
 
 // change floor, input is floor div id
@@ -24,4 +43,9 @@ function change_floor(floor) {
     document.querySelector("#floor3").style.display = "none";
 
     document.querySelector(`#${floor}`).style.display = "block";
+}
+
+//create window on the left
+function info(name, num, desc, path) {
+    console.log(name);
 }
